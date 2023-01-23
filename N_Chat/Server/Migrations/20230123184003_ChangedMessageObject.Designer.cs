d637f5b39bbc825bdb1d8d0d510a2b541e17ebb8
Felix Svensson <90309985+felixsvensson123@users.noreply.github.com>
2023-01-23 20:22:01 +0100

    updatedmigrations

--- N_Chat/Server/Migrations/20230123184003_ChangedMessageObject.Designer.cs
+++ N_Chat/Server/Migrations/20230123184003_ChangedMessageObject.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using N_Chat.Server.Data;
 
@@ -11,9 +12,11 @@ using N_Chat.Server.Data;
 namespace NChat.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230123184003_ChangedMessageObject")]
+    partial class ChangedMessageObject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
