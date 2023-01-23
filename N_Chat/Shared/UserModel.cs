17d46778da4b3e8efd0fa5f1ea7b79201a087f10
Felix Svensson <90309985+felixsvensson123@users.noreply.github.com>
2023-01-23 11:14:10 +0100

    small changs to gitignore for rider users and added space for property

--- N_Chat/Shared/UserModel.cs
+++ N_Chat/Shared/UserModel.cs
@@ -12,7 +12,7 @@ namespace N_Chat.Shared
     public class UserModel : IdentityUser
     {
        public string Email { get; set; }
-       public List<MessageModel>Messages { get; set; }
+       public List<MessageModel> Messages { get; set; }
        public List<ChatModel> Chats { get; set; }
     }
 }
