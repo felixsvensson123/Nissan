a2bdc889ede4437a1d86609f57cbea6f3ac37c0e
Felix Svensson <90309985+felixsvensson123@users.noreply.github.com>
2023-01-24 14:37:11 +0100

    update usercontroller

--- N_Chat/Server/Controllers/UserController.cs
+++ N_Chat/Server/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Identity;
+using N_Chat.Shared.dto;
 
 namespace N_Chat.Server.Controllers
 {
@@ -9,8 +10,29 @@ namespace N_Chat.Server.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-      
 
-        
+        private readonly UserManager<UserModel> userManager;
+        private readonly SignInManager<UserModel> signInManager;
+
+        public UserController(UserManager<UserModel> userManager, SignInManager<UserModel> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(LoginModel user)
+        {
+            if (ModelState.IsValid) //Checks if object is valid
+            {
+                var result = await signInManager.PasswordSignInAsync(user.Username, user.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return Ok(result);
+                }
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt") // Error Message if string is empty
+            }
+            return BadRequest(user);
+        }
     }
 }
