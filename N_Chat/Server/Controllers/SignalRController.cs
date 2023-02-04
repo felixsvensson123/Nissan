309c66094a937b0dd96f3a9dafbf38775c685043
Felix Svensson <90309985+felixsvensson123@users.noreply.github.com>
2023-02-04 15:45:02 +0100

    fixed bugs

--- N_Chat/Server/Controllers/SignalRController.cs
+++ N_Chat/Server/Controllers/SignalRController.cs
@@ -8,66 +8,28 @@ public class SignalRController : Hub
 
 {
     public const string HubUrl = "/conversations";
-   
-    private readonly ChatController _chatController;
-    private readonly MessageController _messageController;  
-    private readonly EncryptionController _encryptionController;    
-    private readonly UserController _userController;    
-
-   
+    
     private readonly DataContext context;
-    private string encryptedMessage;
 
-    //private IPasswordHasher<UserModel> hash;
-    //private HashAlgorithm hash;
-
-    public SignalRController(DataContext context,ChatController chatController,MessageController messageController,UserController userController,EncryptionController encryptionController)
+    public SignalRController(DataContext context)
     {
         this.context = context;
-        this._chatController = chatController;  
-        this._messageController = messageController;    
-        this._userController = userController;  
-        this._encryptionController = encryptionController;  
-       
-        //this.hash = hash;
-      
+        
     }
-
-
-
-
-
-
-
-    //UserSendsChatMessage -to all users
+    
     public async Task SendMessage(string user, string message, string userId)
 
     {
-        
-        await Clients.Client(userId).SendAsync("Broadcast", user, message, userId);
-        //  encryptionController.Encrypt(message);
+        await Clients.All.SendAsync("Broadcast", user, message, userId);
     }
 
     //OneonOne-ChatConveration
-    public  async Task SendPrivateChatMessage(string userId,string user,string message,int chatId,bool isChatEncrypted)
+    public async Task SendPrivateChatMessage(string userId, string user, string message, int chatId,
+        bool isChatEncrypted)
     {
-        await _chatController.GetById(chatId);
-        await Clients.Client(userId).SendAsync("RecivePrivateChatMessage",message, user,userId,chatId, isChatEncrypted);
+        await Clients.Client(userId)
+            .SendAsync("RecivePrivateChatMessage", message, user, userId, chatId, isChatEncrypted);
     }
-
-    public async Task EncryptChat(string message,bool IsChatEncrypted)
-    {
-        if (IsChatEncrypted)
-        {
-            message = _encryptionController.Encrypt(message);
-        }
-        
-      
-    }
-
-
-
-
 
     public override Task OnConnectedAsync()
     {
@@ -80,9 +42,4 @@ public class SignalRController : Hub
         Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
         await base.OnDisconnectedAsync(e);
     }
-    //hash.HashPassword(userModel, messageModel.Message);
-    /*public async Task<ActionResult> Test()
-    {
-    }*/
-    
 }
