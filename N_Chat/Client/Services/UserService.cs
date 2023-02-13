748e06957b980cb79a726a146f59c1409e9b6d23
Felix Svensson <90309985+felixsvensson123@users.noreply.github.com>
2023-02-13 15:13:13 +0100

    fix errror in dev

--- N_Chat/Client/Services/UserService.cs
+++ N_Chat/Client/Services/UserService.cs
@@ -95,7 +95,6 @@ namespace N_Chat.Client.Services
     {
         Task<string> LoginUser(LoginModel loginModel); // Login User Method
         Task<string> SignUp(RegisterModel registerModel); // Signup User Method
-        Task<UserModel> GetUserById(string id);  // Get user by id method
         Task<UserModel> GetUserByName(string name);// Get user by username method
         Task<string> Signout(); // Signout user
         Task<(string Message, UserModel? user)> GetUserClaim(); //Gets user via claims
