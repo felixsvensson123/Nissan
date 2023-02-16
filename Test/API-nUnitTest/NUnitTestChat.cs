5708a94083edd639dcb14d7f8b5d97d7bb499bfb
almafrank <92314215+almafrank@users.noreply.github.com>
2023-02-16 15:55:07 +0100

    test nunitChat

--- Test/API-nUnitTest/NUnitTestChat.cs
+++ Test/API-nUnitTest/NUnitTestChat.cs
@@ -1,7 +1,10 @@
-﻿using N_Chat.Client.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using N_Chat.Client.Services;
+using N_Chat.Server.Controllers;
+using N_Chat.Server.Data;
 using N_Chat.Shared;
-using System.Net.Http;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 
 
@@ -10,218 +13,148 @@ namespace Test.API_nUnitTest
 {
     public class NUnitTestChat
     {
-            [TestFixture]
-            public class ChatTests
-            {
 
-                private HttpClient _httpClient;
-                private IChatService _service;
-                private new List<ChatModel> chatList;
+
+        [TestFixture]
+        public class ChatTestsAll
+        {
+            public HttpClient _httpClient;
+            public DataContext _context;
+            public IChatService _service;
 
             [SetUp]
             public void Setup()
             {
                 _httpClient = new HttpClient();
-                _httpClient.BaseAddress = new Uri("https://localhost:7280/getall");
+                _httpClient.BaseAddress = new Uri("https://localhost:7280/api/chat/getall");
                 _service = new ChatService(_httpClient);
+
+                var options = new DbContextOptionsBuilder<DataContext>()
+                    .UseInMemoryDatabase(databaseName: "nchatdb")
+                    .Options;
+                _context = new DataContext(options);
+                _context.Database.EnsureCreated();
+
+                // Initialize database with test data
+                _context.Chats.Add(new ChatModel { Name = "Chat 1", CreatorId = "", IsChatEdited = false });
+                _context.Chats.Add(new ChatModel { Name = "Chat 2", CreatorId = "", IsChatEdited = false });
+                _context.SaveChanges();
+            }
+
+
+            [TearDown]
+            public void Teardown()
+            {
+                // Clean up database after test is complete
+                _context.Database.EnsureDeleted();
+                _context.Dispose();
+            }
+            [Test]
+            public void TestGetChatsFromDatabase()
+            {
+                // Retrieve chat data from database
+                var chats = _context.Chats.ToList();
+                Assert.Multiple(() =>
+                {
+
+                    // Assert that the retrieved data matches expectations
+                    Assert.That(chats.Count(), Is.EqualTo(2));
+                    Assert.That(chats.ElementAt(0).Name, Is.EqualTo("Chat 1"));
+                    Assert.That(chats.ElementAt(1).Name, Is.EqualTo("Chat 2"));
+                });
+
             }
 
 
 
 
-            [Test]//test to get all chats in db
-                public async Task TestGetAllChats()
-                {
-                    // Act
-                    int expectedChatCount = 2;
-                    
-                    //Arrange
-                    List<ChatModel> chats = await _service.GetChats();
 
 
-                    // Assert
-                    Assert.Multiple(() =>
-                    {
-                        //chats is not empty
-                        Assert.That(chats, Is.Not.Empty);                      
-                        //there is chats in the list
-                        Assert.That(chats.Any(), Is.True);
-                        //contain 10 chat in the list
-                        Assert.AreEqual(expectedChatCount, chats.Count);
-
-                    });
-                }
-                [Test]//admin chat exist in chats db
-                public async Task TestGetChatById()
-                {
-                    // Arrange
-                    int chatId=1;
-                    string creatorId = "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";//admin id
-                    string chatName = "EncryptChatTest";
-                    bool ischatEdit = false;
-                    bool ischatEnded = false;
-                    bool ischatEncrypt = true;
-                   
-                   
-                    ChatModel expectedChat = new() { Id = chatId,CreatorId = creatorId,IsChatEnded=ischatEnded,IsChatEncrypted=ischatEncrypt,IsChatEdited=ischatEdit,Name=chatName};
-
-                    //Act
-                    ChatModel Chatlist = await _service.GetChatById(chatId);
-
-                    // Assert
-                    Assert.AreEqual(expectedChat, Chatlist);
-                }
-
-                [Test]//felix create group chat-encrypt with calin and anna in chats db
-                public async Task TestPostNewGroupChatEncryptedToList()
-                {
-                    //Arrange
-                    
-                    string chatName = "Felix-My new Encrypted Group chat";
-                    bool encrypted = true;
-                    string creatorid= "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";
-                    UserChat userId1 = new() { UserId = "" };
-                    UserChat userId2 = new() { UserId = "" };
-                    List<UserChat> usersInNewChat = new() { userId1, userId2 };
-
-                    ChatModel expectedNewchat = new() { Name = chatName, IsChatEncrypted = encrypted, CreatorId =creatorid,Users=usersInNewChat };
-
-                  
-
-                    List<ChatModel> newChattoChatlist = await _service.GetChats();
-
-                   
-                    Assert.That(newChattoChatlist, Does.Contain(expectedNewchat));
-
-                }
-            
+        }
 
 
-                [Test]// felix create group chat-non encrypt with calin and anna in chats db
-                public async Task TestCreateaNewGroupChatNotEncryptedToList()
-                {
-                string chatName = "Felix-My new Non Encrypt Group chat";
-                bool encrypted = false;
-                string creatorid = "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";
 
-                UserChat userId1 = new() { UserId = "" };
-                UserChat userId2 = new() { UserId = "" };
-                List<UserChat> usersInNewChat = new() { userId1, userId2 };
 
-                ChatModel expectedNewchat = new() { Name = chatName, IsChatEncrypted = encrypted, CreatorId = creatorid, Users = usersInNewChat };
+        [TestFixture]
+        public class ChatTestsGetById
+        {
+            public HttpClient _httpClient;
+            public DataContext _context;
+            public IChatService _service;
+            int id = 1;
 
-               
+            [SetUp]
+            public void Setup()
+            {
+                _httpClient = new HttpClient();
+                _httpClient.BaseAddress = new Uri("https://nissanchat.azurewebsites.net/api/getall");
 
-                List<ChatModel> newChattoNotEncyptToChatlist = await _service.GetChats();
+                _service = new ChatService(_httpClient);
 
-             
-                Assert.That(newChattoNotEncyptToChatlist, Does.Contain(expectedNewchat));
+                var options = new DbContextOptionsBuilder<DataContext>()
+                    .UseInMemoryDatabase(databaseName: "nchatdb")
+                    .Options;
+                _context = new DataContext(options);
+                _context.Database.EnsureCreated();
 
-            }
-                [Test]//a chat is softdeleted from the chat list in db.
-                public async Task TestSoftDeleteAUserChatFromList()
-                {
-               
+                // Initialize database with test data/ find id 1
 
-                    // Arrange
-                    int chatId = 1;
-                    bool isChatSoftDeleted = true;
-                    ChatModel existingChat = await _service.GetChatById(chatId);//hämtar current chatId
 
-                    // Act
-                    ChatModel updatedChat = new() { Id =chatId,IsChatEnded=isChatSoftDeleted };//new updated chat-is deleted
-
-                    _service.UpdateChat(updatedChat, chatId);
-
-                     // Assert
-                    ChatModel newChat = await _service.GetChatById(chatId);
-                    Assert.AreEqual(updatedChat.IsChatEnded, newChat.IsChatEnded);
+                _context.Chats.FirstOrDefault(x => x.Id == id);
+                _context.SaveChanges();
 
 
             }
-                [Test]//
-                public async Task TestUpdateAUserChatFromList()
-                {
-                    //Act
-                    string userId = "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";
-                    int chatId = 1; 
-                    string newChatName = "My New chat Name-Admin Special Chat";
-                    bool isChatEdited = true;
-
-                    //hämtar chatid som ska uppdateras
-                    ChatModel updateChatName = await _service.GetChatById(chatId);
-
-                    //Arrange
-
-                    
-                    ChatModel actualChatWithnewName= new() {Name= newChatName,CreatorId=userId,Id=chatId,IsChatEdited =isChatEdited};
-                    _service.UpdateChat(updateChatName, chatId);
-
-                   ChatModel chatwithNewName= await _service.GetChatById(chatId);
-                   Assert.AreEqual(updateChatName.IsChatEdited,chatwithNewName.IsChatEdited);
-                    
-                   
-
-                 
-                   //Assert
-
-                }
-
-                [Test]//felix create a singel chat-not encrypt with calin in chats db
-                public async Task TestCreateaNewSingelChatNotEncryptedToList()
-                {
-                    string chatName = "Felix-My chat with Calin-NonEncrypt";
-                    bool encrypted = false;
-                    string creatorid = "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";
-
-                   UserChat userId = new() { UserId = "" };
-                   
-                    List<UserChat> usersInNewChat = new() { userId};
-
-                    ChatModel expectedNewSingelchat = new() { Name = chatName, IsChatEncrypted = encrypted, CreatorId = creatorid, Users = usersInNewChat };
-
-                    
-
-                    List<ChatModel> newSingelChatToChatlist = await _service.GetChats();
-
-                    
-                    Assert.That(newSingelChatToChatlist, Does.Contain(expectedNewSingelchat));
-
-                }
-
-                [Test]//felix create singel chat-encrypt with calin in chats db
-                public async Task TestCreateNewSingelEncryptedChatToList()
-                {
-                string chatName = "Felix-My chat with Calin-Encrypt";
-                bool encrypted = true;
-                string creatorid = "d7fc4ba6-4957-41a7-96b5-52b65c06bc35";
-
-                UserChat userId = new() { UserId = "" };
-
-                List<UserChat> usersInNewChat = new() { userId };// adding new usser to our chat
-
-                ChatModel expectedNewSingelchat = new() { Name = chatName, IsChatEncrypted = encrypted, CreatorId = creatorid, Users = usersInNewChat };// creating a new ccha¨t with properties
-
-                //Arrange
-
-                List<ChatModel> newSingelChatToChatlist = await _service.GetChats(); // fetching chatlist via service that connected to controller
-
-                //Assert
-                Assert.That(newSingelChatToChatlist, Does.Contain(expectedNewSingelchat));// testing if the new chatexist in chatlist.
-                }
-
-
+            [TearDown]
+            public void Teardown()
+            {
+                // Clean up database after test is complete
+                _context.Database.EnsureDeleted();
+                _context.Dispose();
             }
 
 
+            [Test]
+            public async Task TestGetAChatFromDatabase()
+            {
+                // Arrange
+                int id = 1;
 
-       
 
-            
+                // Act
+                var chat = await _service.GetChatById(id);
+                Console.WriteLine(chat);
+
+                // Assert
+                Assert.That(chat, Is.Not.Null);
+                Assert.Multiple(() =>
+                {
+                    Assert.That(chat.Id, Is.EqualTo(id));
+
+                    Assert.That(_httpClient.BaseAddress + $"chat/getbyid/{id}", // Update URL to include correct endpoint
+                        Is.EqualTo("https://localhost:7280/api/chat/getbyid/1"));
+
+                });
+            }
 
 
-           
-        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+    
 }
 
